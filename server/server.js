24b336d61f952c93de1a065e39525fd02a5cc04c
Odumah Solomon <odumahs@gmail.com>
2018-04-12 16:17:58 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -70,6 +71,35 @@ app.delete('/todos/:id', (req, res) =>{
 		return res.status(400).send();
 	});
 
+});
+
+//update route
+app.patch('/todos/:id', (req, res) =>{
+	let id = req.params.id; 
+	let body = _.pick(req.body, ['text', 'completed']);
+
+	//validate id
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
+		if (!todo) {
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+
+	}).catch((e) => {
+		res.status(400).send();
+	});
 });
 
 app.listen(port, () =>{
